74c19f1c4c0a6a5cba2b895135733c28e569d48a
ike <blazej@wodecki.dev>
2023-03-09 20:01:14 +0100

    fix: exit app on mobile by tapping dynamicisland

--- src/components/Interface/SystemLayout/DynamicIsland.tsx
+++ src/components/Interface/SystemLayout/DynamicIsland.tsx
@@ -1,6 +1,16 @@
+import { interfaceActions } from "@/app/interface";
+import { useDispatch } from "react-redux";
+
 const DynamicIsland = () => {
+  const dispatch = useDispatch();
+
   return (
-    <div className="z-50 absolute left-1/2 -translate-x-1/2 w-3/12 h-7 rounded-3xl top-3 bg-black" />
+    <div
+      onClick={() => {
+        dispatch(interfaceActions.exitApp());
+      }}
+      className="z-50 absolute left-1/2 -translate-x-1/2 w-3/12 h-7 rounded-3xl top-3 bg-black pointer-events-auto"
+    />
   );
 };
 
